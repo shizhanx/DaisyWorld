9fc676d5f994d407fa3c5caa65fb6c93a6a17101
Shizhan Xu <copernicus.xu@gmail.com>
2021-04-27 15:59:53 +0800

    Initial commit

---
+++ src/DaisyWorld/Daisy.java
@@ -0,0 +1,4 @@
+package DaisyWorld;
+
+public class Daisy {
+}
---
+++ src/DaisyWorld/Params.java
@@ -0,0 +1,10 @@
+package DaisyWorld;
+
+public class Params {
+    public static double ALBEDO_OF_WHITE = 0.7;
+    public static double ALBEDO_OF_BLACK = 0.3;
+    public static double ALBEDO_OF_GROUND = 0.4;
+    public static double LUMINOSITY = 1.0;
+    public static double START_WHITE = 0.4;
+    public static double START_BLACK = 0.4;
+}
