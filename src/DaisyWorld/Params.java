9c24fb675b6621786d1779a0390a4ccd0c0b0d23
Shizhan Xu <copernicus.xu@gmail.com>
2021-04-27 16:13:19 +0800

    Finalize Params.java

--- src/DaisyWorld/Params.java
+++ src/DaisyWorld/Params.java
@@ -1,10 +1,16 @@
 package DaisyWorld;
 
 public class Params {
-    public static double ALBEDO_OF_WHITE = 0.7;
-    public static double ALBEDO_OF_BLACK = 0.3;
+    // How much light energy is deflected from the ground
+    public static double ALBEDO_OF_WHITE = 0.75;
+    public static double ALBEDO_OF_BLACK = 0.25;
     public static double ALBEDO_OF_GROUND = 0.4;
+    // Initial luminosity of the sun, 1.0 is the default
     public static double LUMINOSITY = 1.0;
-    public static double START_WHITE = 0.4;
-    public static double START_BLACK = 0.4;
+    // Starting percentage of each kind of daisies. No more than 50%
+    public static double START_WHITE = 0.2;
+    public static double START_BLACK = 0.2;
+
+    public enum DAISY_COLOUR { black, white }
+    public enum SCENARIO { Ramp, maintain, low, our, high }
 }
